483faf09bab37bc8f0a6377543f1973a683aec6f
Rishabh <rishabh@Rishabhs-MacBook-Air.local>
2025-03-05 16:46:31 +0530

    file name changes

--- src/services/productService.jsx
+++ src/services/productService.jsx
@@ -1,5 +1,5 @@
 import axios from '@/config/axios'
-import API_CONFIG from '@/config/ApiConfig'
+import API_CONFIG from '@/config/apiConfig'
 
 export const fetchProducts = () => {
     const res = axios.get(API_CONFIG.products.getProducts);
